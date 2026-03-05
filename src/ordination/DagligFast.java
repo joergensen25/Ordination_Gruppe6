aad31faa3eb4d8c3c7aea2c7234b72ab3d848fd7
haon02 <noahvad@gmail.com>
2026-03-05 09:57:25 +0100

    createDosis lavet

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,25 @@
 package ordination;
 
+import java.lang.reflect.Array;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class DagligFast {
-    // TODO
+    private final Dosis[] doser = new Dosis[4];
+    private int antalDoser = 0;
+
+    public Dosis[] getDoser() {
+        return doser;
+    }
+
+public Dosis createDosis(LocalTime tid, double antal) {
+    if (antalDoser < 4) {
+        Dosis dosis = new Dosis(tid, antal);
+        doser[antalDoser] = dosis;
+        antalDoser++;
+        return dosis;
+    } else {
+        return null;
+    }
+}
 }
