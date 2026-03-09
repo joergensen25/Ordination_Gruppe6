b626f67b24e0c6f3987b0089db5c3a02650960f7
haon02 <noahvad@gmail.com>
2026-03-09 10:04:26 +0100

    anbefaletDosisPrDoegnNormal,anbefaletDosisPrDoegnLet, anbefaletDosisPrDoegnTung

--- Test/DagligFastTest.java
+++ Test/DagligFastTest.java
@@ -1,9 +1,51 @@
+import controller.Controller;
+import ordination.Dosis;
+import ordination.Laegemiddel;
+import ordination.Ordination;
+import ordination.Patient;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DagligFastTest {
 
     @Test
-    void doegnDosis(){
+    void anbefaletDosisPrDoegnNormal() {
+        Controller controller = Controller.getTestController();
+        Patient patient = new Patient("121110-8765", "Rasmus", 80.00);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Styk");
+        //Act
+        double expected = 120.0;
+        double actual = controller.anbefaletDosisPrDoegn(patient, laegemiddel);
+        //Assert
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void anbefaletDosisPrDoegnLet() {
+        Controller controller = Controller.getTestController();
+        Patient patient = new Patient("121110-8765", "Rasmusine", 15.00);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Styk");
+        //Act
+        double expected = 15.0;
+        double actual = controller.anbefaletDosisPrDoegn(patient, laegemiddel);
+        //Assert
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void anbefaletDosisPrDoegnTung() {
+        Controller controller = Controller.getTestController();
+        Patient patient = new Patient("121110-8765", "Noah", 126);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Styk");
+        //Act
+        double expected = 252.0;
+        double actual = controller.anbefaletDosisPrDoegn(patient, laegemiddel);
+        //Assert
+        assertEquals(expected, actual);
 
     }
 }
