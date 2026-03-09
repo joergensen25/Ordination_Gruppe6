45e1223c99f81536aa00a4d34813c4b07a5f63b7
rasjo <rasmus.joergensen@outlook.com>
2026-03-09 13:12:41 +0100

    TestAntalDage oprettet

--- Test/OrdinationAntalDageTest.java
+++ Test/OrdinationAntalDageTest.java
@@ -6,16 +6,31 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OrdinationAntalDageTest {
 
     @Test
     void testAntalDageSammeDag() {
         Laegemiddel lm = new Laegemiddel("Test", 1, 1, 1, "stk");
         Patient patient = new Patient("111111-1111", "Testperson", 67);
-        PN pn = new PN(LocalDate.of(2026, 1, 1), LocalDate.of(2026, 1, 1), patient);
+        PN pn = new PN(
+                LocalDate.of(2026, 1, 1),
+                LocalDate.of(2026, 1, 1),
+                patient);
+
+        assertEquals(1, pn.antalDage());
     }
     @Test
     void testAntalDageFlereDage() {
+        Laegemiddel lm = new Laegemiddel("Test", 1, 1, 1, "stk");
+        Patient patient = new Patient("111111-1111", "Testperson", 67);
+        PN pn = new PN(
+                LocalDate.of(2026, 1, 1),
+                LocalDate.of(2026, 1, 3),
+                patient);
+
+        assertEquals(3, pn.antalDage());
 
     }
 
