1628dd890732922d1e52d30e79cf052a6fc04e1c
rasjo <rasmus.joergensen@outlook.com>
2026-03-09 09:35:05 +0100

    Tilføjet Laegemiddel som parameter til Ordination subklasser

---
+++ src/ControllerAnbefaletDosisTest.java
@@ -0,0 +1,14 @@
+import controller.Controller;
+import ordination.Laegemiddel;
+import org.junit.jupiter.api.Test;
+
+public class ControllerAnbefaletDosisTest {
+
+    private Controller controller;
+    private Laegemiddel laegemiddel;
+
+    @Test
+    void testAnbefaletDosisLetPatient() {
+
+    }
+}
